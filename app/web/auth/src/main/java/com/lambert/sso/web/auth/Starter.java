2c053e686e401d9fd6c643766ab4218c9165f3f5
林泽宽 <linzekuan@163.com>
2020-07-30 21:49:59 +0800

    pom

---
+++ app/web/auth/src/main/java/com/lambert/sso/web/auth/Starter.java
@@ -0,0 +1,17 @@
+package com.lambert.sso.web.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author lambert
+ * @version $Id: Starter.java, v 0.1 2020年07月30日 9:43 PM lambert Exp $
+ */
+@SpringBootApplication
+public class Starter extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class, args);
+    }
+}
